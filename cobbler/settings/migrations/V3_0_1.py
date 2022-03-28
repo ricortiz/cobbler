062941140909eed0eead95d4ea645c2e421dca68
Enno Gotthold <egotthold@suse.de>
2022-03-28 09:50:20 +0200

    WIP: Integrate modules.conf migration
    This uses the regex replacement approach to automatically migrate to the new
    modules.conf version.

--- cobbler/settings/migrations/V3_0_1.py
+++ cobbler/settings/migrations/V3_0_1.py
@@ -5,7 +5,7 @@ Migration from V3.0.0 to V3.0.1
 # SPDX-FileCopyrightText: 2021 Dominik Gedon <dgedon@suse.de>
 # SPDX-FileCopyrightText: 2021 Enno Gotthold <egotthold@suse.de>
 # SPDX-FileCopyrightText: Copyright SUSE LLC
-
+import re
 
 from schema import SchemaError
 from cobbler.settings.migrations import V3_0_0
@@ -45,7 +45,25 @@ def migrate(settings: dict) -> dict:
     :param settings: The settings dict to migrate
     :return: The migrated dict
     """
-    # TODO: modules.conf migration
+    modules_conf_path = "/etc/cobbler/modules.conf"
+    with open(modules_conf_path, "r") as modules_conf_file:
+        modules_conf = modules_conf_file.readlines()
+
+    for line in modules_conf:
+        if re.match("authn_", line) is not None:
+            # sed -i -e 's/authn_/authentication\./g' $path_modules_conf
+            pass
+        if re.match("authz_", line) is not None:
+            # sed -i -e 's/authz_/authorization\./g' $path_modules_conf
+            pass
+        if re.match("manage_", line) is not None:
+            # sed -i -e 's/manage_/managers\./g' $path_modules_conf
+            pass
+
+    with open(modules_conf_path, "w") as modules_conf_file:
+        for line in modules_conf:
+            modules_conf_file.write(line)
+
     if not validate(settings):
         raise SchemaError("V3.0.1: Schema error while validating")
     return normalize(settings)
